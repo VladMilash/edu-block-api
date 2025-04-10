630e7fbb8171a90d306d31b9852cb0507bbd4859
VladMilash <12milash12@gmail.com>
2025-04-10 15:26:40 +0300

    Fixed StudentServiceImplTest

--- src/test/java/com/mvo/edublockapi/service/impl/StudentServiceImplTest.java
+++ src/test/java/com/mvo/edublockapi/service/impl/StudentServiceImplTest.java
@@ -10,21 +10,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
-
-import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
